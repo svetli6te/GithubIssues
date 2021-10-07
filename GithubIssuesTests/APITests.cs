8af4d3794183c329534453d8926d7672d8c0cde2
Svetla Chavdarova Manolova <svetla4manolova@gmail.com>
2021-10-07 11:13:13 +0300

    Project Setup

---
+++ GithubIssuesTests/APITests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GithubIssuesTests
+{
+    [TestClass]
+    public class APITests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
